a2d77728a0bde17b1dea7516d3ee5f054423f323
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-03-07 11:15:50 -0800

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo '<html>
+  <head>
+  </head>
+  <body>
+    AirBnB Clone v2
+  </body>
+</html>' > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+service nginx restart
