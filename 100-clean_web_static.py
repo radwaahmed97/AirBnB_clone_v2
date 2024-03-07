5a54b9b45ba554a6ffb5ddab3618ac0675dbbb1b
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-03-07 13:45:38 -0800

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -51,3 +51,18 @@ def deploy():
         return False
     result = do_deploy(newarchive_path)
     return result
+
+
+def do_clean(number=0):
+    ''' Removes out of date archives locally and remotely '''
+    number = int(number)
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions; ls -t | tail -n +{} | xargs rm -rf'
+          .format(number))
+    releases_path = '/data/web_static/releases'
+    run('cd {}; ls -t | tail -n +{} | xargs rm -rf'
+        .format(releases_path, number))
