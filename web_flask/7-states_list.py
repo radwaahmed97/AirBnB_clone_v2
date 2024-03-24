d5aa78a14ff13e7d5fe725e14e0998688a5d2494
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-03-24 16:05:42 -0400

    7-states_list.html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ def close_db(error):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ displays HTML page with a list of states """
-    states = storage.all(State).values()
+    states = list(storage.all(State).values())
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
 
