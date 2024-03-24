70e6a5622d87b5ddedf49ab0c9876c9f90e86324
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-03-24 14:36:22 -0400

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,17 +2,15 @@
 """script that starts a Flask web application"""
 from flask import Flask
 
-
 applicaiton = FLask(__name__)
 """flask application instance"""
 application.url_map.strict_slashes = False
 
 
-
 @application.route('/')
 def index():
-"""The home page"""
-return 'Hello HBNB!'
+    """The home page"""
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
