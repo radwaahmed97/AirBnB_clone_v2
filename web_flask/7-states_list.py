094bdc62304f57ba0fac712e77ffe2298399fe3a
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-03-24 15:58:37 -0400

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage, storgae.all()
+from models import storage
 from models.state import State
 app = Flask(__name__)
 
