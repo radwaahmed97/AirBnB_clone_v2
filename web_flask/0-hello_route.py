dabe937d0f29b2ab24522aad065d8a0be0e5696f
radwaahmed97 <radwa.ahmed95.com>
2024-03-24 18:34:44 +0000

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+applicaiton = FLask(__name__)
+"""flask application instance"""
+application.url_map.strict_slashes = False
+
+
+
+@application.route('/')
+def index():
+"""The home page"""
+return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port='5000')
