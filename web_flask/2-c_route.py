e52b55683771b34dfbae64f308bfaf449572d16c
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-03-24 15:05:10 -0400

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ prints "Hello HBNB!" when / called """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints "HBNB" when /hbnb called """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Prints C is fun when /c called """
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
